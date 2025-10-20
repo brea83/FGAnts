43c5735cdec4a9f13333e46f5195ebbda2d5c481
Briana <brea83@gmail.com>
2025-10-20 13:51:15 +0200

    Initial commit

---
+++ Source/FGAnts/FGAnts.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FGAnts.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FGAnts, "FGAnts" );
+
+DEFINE_LOG_CATEGORY(LogFGAnts)
+ 
