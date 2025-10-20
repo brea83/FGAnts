7ef060e9e4a40a4d6c71d99ba17116dabc026bc5
Briana <brea83@gmail.com>
2025-10-20 14:05:11 +0200

    empty classes and planning

---
+++ Source/FGAnts/AntGrid.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AntGrid.h"
+
+AntGrid::AntGrid()
+{
+}
+
+AntGrid::~AntGrid()
+{
+}
---
+++ Source/FGAnts/AntNode.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AntNode.h"
+
+AntNode::AntNode()
+{
+}
+
+AntNode::~AntNode()
+{
+}
